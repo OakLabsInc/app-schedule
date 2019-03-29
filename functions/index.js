ed3f39bd4288141ccaf1c642e26a79eff5792643
Richard Joffray <rjoffray@richards-mbp.lan>
2019-03-29 09:36:07 -0500

    ran standard js

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,31 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
-var ical2json = require("ical2json");
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
+var ical2json = require('ical2json')
 var ICAL = require('ical')
-var request = require("request");
+var request = require('request')
 const cors = require('cors')({
-  origin: true,
-});
+  origin: true
+})
 
-function parseIcal(data) {
-  console.log("###########################", new Date(), "###########################")
+function parseIcal (data) {
+  console.log('###########################', new Date(), '###########################')
   let type = 'apple/office'
-  if(data.indexOf('Google') > -1) {
-    type='google'
+  if (data.indexOf('Google') > -1) {
+    type = 'google'
   }
   var jcalData = ICAL.parseICS(data)
   var events = []
-  for(var i in jcalData){
+  for (var i in jcalData) {
     let event = {}
-    if(jcalData[i].type.toLowerCase() === 'vevent') {
-      if(type !== 'google'){
+    if (jcalData[i].type.toLowerCase() === 'vevent') {
+      if (type !== 'google') {
         events.push({
           type: type,
           start: jcalData[i].start.toLocaleString('en-US', { timeZone: 'UTC' }),
           end: jcalData[i].end.toLocaleString('en-US', { timeZone: 'UTC' }),
           summary: jcalData[i].summary || '',
-          description: jcalData[i].description ||''
+          description: jcalData[i].description || ''
         })
       } else {
         events.push({
@@ -33,7 +33,7 @@ function parseIcal(data) {
           start: jcalData[i].start.toISOString(),
           end: jcalData[i].end.toISOString(),
           summary: jcalData[i].summary || '',
-          description: jcalData[i].description ||''
+          description: jcalData[i].description || ''
         })
       }
     }
@@ -42,54 +42,54 @@ function parseIcal(data) {
 }
 
 exports.convertIcs2Json = functions.https.onRequest((request, response) => {
-  console.log("Request: ", request.body.ics)
-  var jsonData = ICAL.parseICS(request.body.ics);
-  response.json(jsonData);
-  //response.send("Hello from convertIcs2Json!");
-});
+  console.log('Request: ', request.body.ics)
+  var jsonData = ICAL.parseICS(request.body.ics)
+  response.json(jsonData)
+  // response.send("Hello from convertIcs2Json!");
+})
 
 exports.getIcsContentsFromUrl = functions.https.onRequest((req, res) => {
-  console.log("Request: ", req.body)
+  console.log('Request: ', req.body)
   var body = req.body
   cors(req, res, () => {
     request({
       method: 'GET',
       uri: body.url
     }).pipe(res)
-  });
-});
+  })
+})
 
 exports.addCalendarSubscriptionForUser = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
-    console.log("Request: ", req.body)
+    console.log('Request: ', req.body)
     var kbody = req.body
     let setData = {
       url: kbody.calendarUrl,
       name: kbody.name,
       user: kbody.user
     }
-    console.log("setData: ", setData)
-    admin.firestore().collection('subscriptions').doc(kbody.user).collection('schedules').doc(kbody.name).set(setData).then( function () {
-      res.send("Subscription Written: ")
-        let events = []
-        request({
-          method: 'GET',
-          uri: kbody.calendarUrl,
-          followAllRedirects: true
-        }, function (error, response, body) {
-          if (error) {
-            return console.error('upload failed:', error);
-          }
-          let eventsParsed = parseIcal(body)
-          kbody.events = eventsParsed
-          console.log('Upload successful!  Server responded with:',kbody);
-          admin.firestore().collection('users').doc(kbody.user).collection('schedules').doc(kbody.name).set(kbody)
-        })
+    console.log('setData: ', setData)
+    admin.firestore().collection('subscriptions').doc(kbody.user).collection('schedules').doc(kbody.name).set(setData).then(function () {
+      res.send('Subscription Written: ')
+      let events = []
+      request({
+        method: 'GET',
+        uri: kbody.calendarUrl,
+        followAllRedirects: true
+      }, function (error, response, body) {
+        if (error) {
+          return console.error('upload failed:', error)
+        }
+        let eventsParsed = parseIcal(body)
+        kbody.events = eventsParsed
+        console.log('Upload successful!  Server responded with:', kbody)
+        admin.firestore().collection('users').doc(kbody.user).collection('schedules').doc(kbody.name).set(kbody)
+      })
     })
-  });
-});
+  })
+})
 
-function updateSubscription(subscription){
+function updateSubscription (subscription) {
   return new Promise((resolve, reject) => {
     request({
       method: 'GET',
@@ -97,40 +97,40 @@ function updateSubscription(subscription){
       followAllRedirects: true
     }, function (error, response, body) {
       if (error) {
-        console.error('request failed:', error);
+        console.error('request failed:', error)
         return reject(error)
       }
       let eventsParsed = parseIcal(body)
-      console.log('Parse successful!', eventsParsed);
+      console.log('Parse successful!', eventsParsed)
       admin.firestore().collection('users').doc(subscription.user).collection('schedules').doc(subscription.name).update({
         events: eventsParsed
       }).then(
-        function(){
-          console.log("Updated: ",subscription.name)
+        function () {
+          console.log('Updated: ', subscription.name)
           resolve()
         },
         function (err) {
           reject(err)
-        } 
+        }
       )
     })
   })
 }
 
 exports.updateUserScheduleSubscriptions = functions.https.onRequest((req, res) => {
-  cors(req, res,async function () {
+  cors(req, res, async function () {
     const subscriptions = await admin.firestore().collection('subscriptions').get()
-    
+
     const subscriptionsArray = []
     for (const user in subscriptions.docs) {
       const schedules = await subscriptions.docs[user].ref.collection('schedules').get()
-        for (const schedule in schedules.docs) {
-          const data = await schedules.docs[schedule].data()
-          subscriptionsArray.push(data)
-        }
+      for (const schedule in schedules.docs) {
+        const data = await schedules.docs[schedule].data()
+        subscriptionsArray.push(data)
+      }
     }
-    
-    for(var subscription in subscriptionsArray){
+
+    for (var subscription in subscriptionsArray) {
       await updateSubscription(subscriptionsArray[subscription])
     }
     console.log('done with everything')
--- src/public/index.js
+++ src/public/index.js
@@ -9,7 +9,7 @@ var app = window.angular
   .constant('os', window.os)
   .constant('oak', window.oak)
   .constant('_', window._)
-  
+
   .config(function ($sceDelegateProvider) {
     $sceDelegateProvider.resourceUrlWhitelist(['self'])
   })
@@ -41,23 +41,21 @@ app.controller('appController', function ($log, $sce, $timeout, $scope, $http, $
 
     db.collection('users').doc($scope.environment.apiKey).collection('schedules').doc($scope.environment.scheduleName)
       .onSnapshot(function (doc) {
-        
         var source = doc.metadata.hasPendingWrites ? 'Local' : 'Server'
         console.log(source, ' data: ', doc.data())
         $timeout(function () {
-          if (!$scope.schedule){
+          if (!$scope.schedule) {
             $scope.initApp(doc)
           } else {
             oak.reload()
           }
-
         })
       })
-    }, function errorCallback (response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    })
-    
+  }, function errorCallback (response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  })
+
   $scope.initApp = function (doc) {
     $scope.schedule = doc.data()
   }
@@ -66,41 +64,38 @@ app.controller('appController', function ($log, $sce, $timeout, $scope, $http, $
     return $sce.trustAsHtml(markdown.toHTML(text))
   }
 
-  $scope.formatDateString = function(date, format) {
+  $scope.formatDateString = function (date, format) {
     return moment(date).format(format)
   }
 
-  $scope.sortStartDate = function(item){
-    var date = new Date(item.start);
-    return date;
+  $scope.sortStartDate = function (item) {
+    var date = new Date(item.start)
+    return date
   }
 
-  $scope.getTodayDate = function(){
-    return moment(Date.now()).format("MM-DD-YYYY")
+  $scope.getTodayDate = function () {
+    return moment(Date.now()).format('MM-DD-YYYY')
   }
 
-  $scope.betweenFilter = function(item) {
-    return moment(item.start).isBetween(moment($scope.schedule.startDate).format("MM-DD-YYYY"), moment($scope.schedule.endDate).add(1,'days').format("MM-DD-YYYY"))
+  $scope.betweenFilter = function (item) {
+    return moment(item.start).isBetween(moment($scope.schedule.startDate).format('MM-DD-YYYY'), moment($scope.schedule.endDate).add(1, 'days').format('MM-DD-YYYY'))
   }
 
-  $scope.displayCurrentProgress = function(item, index) {
-
+  $scope.displayCurrentProgress = function (item, index) {
     let now = moment().tz($scope.environment.timezone)
     let start = moment(item.start).tz($scope.environment.timezone)
     let end = moment(item.end).tz($scope.environment.timezone)
-   
+
     let shouldShow = false
 
-    if(now.isBetween(start, end) && !$scope.foundItem[index]) {
-      
+    if (now.isBetween(start, end) && !$scope.foundItem[index]) {
       shouldShow = true
 
-      $log.info(" ########################################## ")
-      $log.info("now: ", now.format())
-      $log.info("start: ", start.format())
-      $log.info("end: ", end.format())
+      $log.info(' ########################################## ')
+      $log.info('now: ', now.format())
+      $log.info('start: ', start.format())
+      $log.info('end: ', end.format())
 
-      
       $scope.foundItem[index] = {
         item: item,
         timer: setInterval($scope.setProgress, 1000, index)
@@ -111,30 +106,26 @@ app.controller('appController', function ($log, $sce, $timeout, $scope, $http, $
     } else {
       return false
     }
-
   }
-  $scope.setProgress = function(index) {
-      let item = $scope.foundItem[index].item
-      let now = moment().tz($scope.environment.timezone)
-      let start = moment(item.start)
-      let end = moment(item.end)
-      let full = end.diff(start, 'minutes')
-      let nowMinutes = now.diff(start, 'minutes')
-      $timeout(function(){
-        $scope.timeProgress[index] = nowMinutes*100/full 
-        if($scope.timeProgress[index] == 100 || $scope.isAllDay(item)) {
-          clearInterval($scope.foundItem[index].timer)
-        }
-      })
-
+  $scope.setProgress = function (index) {
+    let item = $scope.foundItem[index].item
+    let now = moment().tz($scope.environment.timezone)
+    let start = moment(item.start)
+    let end = moment(item.end)
+    let full = end.diff(start, 'minutes')
+    let nowMinutes = now.diff(start, 'minutes')
+    $timeout(function () {
+      $scope.timeProgress[index] = nowMinutes * 100 / full
+      if ($scope.timeProgress[index] == 100 || $scope.isAllDay(item)) {
+        clearInterval($scope.foundItem[index].timer)
+      }
+    })
   }
 
-  
-
-  $scope.startCase = function(str) {
+  $scope.startCase = function (str) {
     return _.startCase(str)
   }
-  $scope.isAllDay = function(item) {
+  $scope.isAllDay = function (item) {
     return $scope.formatDateString(item.start, 'h:mm a') === $scope.formatDateString(item.end, 'h:mm a')
   }
 
